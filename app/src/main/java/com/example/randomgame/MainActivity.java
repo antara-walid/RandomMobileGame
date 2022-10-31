7e4663252ecaccb54e6f9f4de014ff8beda779ce
antara-walid <Antara.walid.27@gmail.com>
2022-10-31 08:38:56 +0100

    experiment with mainActivity

--- app/src/main/java/com/example/randomgame/MainActivity.java
+++ app/src/main/java/com/example/randomgame/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.randomgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonGuess;
+    private EditText editTextNumber;
+    private TextView textViewMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGuess = findViewById(R.id.buttonGuess);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        textViewMessage = findViewById(R.id.textViewMessage);
+
+        buttonGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(editTextNumber.getText().toString());
+                int result = number *2;
+                textViewMessage.setText(String.valueOf(result ) );
+            }
+        });
     }
 }
